6584f371abec48f7a2b5c473a0e9ee6dc118ce65
gopal1166 <pgopal1166@gmail.com>
2018-08-20 14:39:40 +0530

    Updated axios.js interceptors

--- react-auth/src/axios.js
+++ react-auth/src/axios.js
@@ -7,6 +7,19 @@ const instance = axios.create({
 
 // Alter defaults after instance has been created
 // instance.defaults.headers.common['Authorization'] = 'Token 1f45cf96b952060ac6a7790415aa382fb9d7f67e';
-instance.defaults.headers.common['Authorization'] = 'Token ' + localStorage.getItem('token');
+//instance.defaults.headers.common['Authorization'] = 'Token ' + localStorage.getItem('token');
+
+instance.interceptors.request.use(function(config) {
+  const token = localStorage.getItem("token");
+
+  if ( token != null ) {
+    config.headers.Authorization = `Token ${token}`;
+  }
+
+  return config;
+}, function(err) {
+  return Promise.reject(err);
+});
+
 
 export default instance;
